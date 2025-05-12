3c89fbb154fdbd91d4372df9130e0ece2b5fb757
raulsb <rsorianobravo@gmail.com>
2025-05-12 09:36:59 +0200

    [ADD] Added main Book Controller

---
+++ BookStoreCatalog_API/Controllers/BookController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookStoreCatalog_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BookController : ControllerBase
+    {
+        //----------------------------------------------
+
+        //----------------------------------------------
+
+        /// <summary>
+        /// Constructor - Injection
+        /// </summary>
+        public BookController()
+        {
+            
+        }
+
+        //----------------------------------------------
+
+    }
+}
