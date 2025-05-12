3245c795967b3eb82c0ec18db6b12fffad1b0dde
raulsb <rsorianobravo@gmail.com>
2025-05-12 11:26:36 +0200

    [ADD] Added local Storage

---
+++ BookStoreCatalog_API/DataStore/BookDataStore.cs
@@ -0,0 +1,15 @@
+﻿using BookStoreCatalog_API.Models.DTO;
+
+namespace BookStoreCatalog_API.DataStore
+{
+    public class BookDataStore
+    {
+        public List<BookModelDTO> bookList = new List<BookModelDTO> 
+        {
+            new BookModelDTO{Id=991,Title="Test5"},
+            new BookModelDTO{Id=992,Title="Test6"}            
+        };
+
+        public BookDataStore() { }
+    }
+}
