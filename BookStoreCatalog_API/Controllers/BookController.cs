fee832d641ecf643970a9201b8a0b2c2a981d4bf
raulsb <rsorianobravo@gmail.com>
2025-05-13 08:27:51 +0200

    [ADD] Added HttpPost

--- BookStoreCatalog_API/Controllers/BookController.cs
+++ BookStoreCatalog_API/Controllers/BookController.cs
@@ -27,7 +27,7 @@ namespace BookStoreCatalog_API.Controllers
         /// (TEST) Get all the Books
         /// </summary>
         /// <returns> A fake list of Books </returns>
-        [HttpGet]
+        [HttpGet("TEST/")]
         [ProducesResponseType(StatusCodes.Status200OK)]
         public ActionResult<IEnumerable<BookModel>> GetAllTest()
         {
@@ -69,11 +69,23 @@ namespace BookStoreCatalog_API.Controllers
 
         //----------------------------------------------
         /// <summary>
+        ///  Get all the Books
+        /// </summary>
+        /// <returns> The list of Books </returns>
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public ActionResult<IEnumerable<BookModel>> GetAll()
+        {
+            return Ok(BookDataStore.bookList);
+        }
+
+        //----------------------------------------------
+        /// <summary>
         /// Get the book by ID
         /// </summary>
         /// <param name="id"></param>
         /// <returns>the book that match with the ID passed </returns>
-        [HttpGet("{id}:int")]
+        [HttpGet("{id}:int", Name = "GetBook")]
         [ProducesResponseType(StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
@@ -92,6 +104,36 @@ namespace BookStoreCatalog_API.Controllers
             }
 
             return Ok(book);
+        }
+
+        //----------------------------------------------
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public ActionResult<BookModelDTO> CreateBook( [FromBody] BookModelDTO newBook)
+        {
+            if (newBook == null)
+            {
+                return BadRequest(newBook);
+            }
+            if (newBook.Id > 0)
+            { 
+                return StatusCode(StatusCodes.Status500InternalServerError); 
+            }
+
+            else
+            {
+                newBook.Id = BookDataStore.bookList.OrderByDescending(book => book.Id).FirstOrDefault().Id + 1;
+                if (BookDataStore.bookList.Any(
+                    book => book.Id == newBook.Id
+                ))
+                {
+                    return BadRequest(newBook);
+                }
+                BookDataStore.bookList.Add(newBook);
+            }
+            return CreatedAtRoute("GetBook", new { id = newBook.Id }, newBook);
         }
     }
 }
