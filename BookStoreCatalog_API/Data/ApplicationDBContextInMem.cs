2eee6a943cdc51c60bb08d6a32425e933b6a1360
raulsb <rsorianobravo@gmail.com>
2025-05-15 11:05:08 +0200

    [ADD] Added init of Db data.

--- BookStoreCatalog_API/Data/ApplicationDBContextInMem.cs
+++ BookStoreCatalog_API/Data/ApplicationDBContextInMem.cs
@@ -6,5 +6,27 @@ namespace BookStoreCatalog_API.Data
     public class ApplicationDBContextInMem(DbContextOptions<ApplicationDBContextInMem> options) : DbContext(options)
     {
         public DbSet<BookModel> Books { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+            base.OnModelCreating(modelBuilder);
+            
+            modelBuilder.Entity<BookModel>().HasData(
+                new BookModel()
+                {
+                    Id = 1,
+                    Title = "Title991",
+                    TitleUrl = "URL991",
+                    Author = "Author991",
+                    AuthorUrl = "URL991",
+                    Description = "Description991",
+                    DescriptionUrl = "URL991",
+                    CreatedAt = DateTime.Now,
+                }
+            );
+
+
+        }
     }
 }
