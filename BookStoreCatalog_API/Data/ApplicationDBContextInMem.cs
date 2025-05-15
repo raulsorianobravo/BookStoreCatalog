fc2afab93ed489a168260325a0a7e16912a734f2
raulsb <rsorianobravo@gmail.com>
2025-05-15 09:11:58 +0200

    [ADD] Added ApplicationDbContext for InMemory and cereate the DbSet of Books based in the model.

---
+++ BookStoreCatalog_API/Data/ApplicationDBContextInMem.cs
@@ -0,0 +1,10 @@
+﻿using BookStoreCatalog_API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookStoreCatalog_API.Data
+{
+    public class ApplicationDBContextInMem(DbContextOptions<ApplicationDBContextInMem> options) : DbContext(options)
+    {
+        public DbSet<BookModel> Books { get; set; }
+    }
+}
