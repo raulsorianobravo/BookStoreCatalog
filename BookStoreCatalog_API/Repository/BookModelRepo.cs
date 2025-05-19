4eebb0443105369d2d471c2cab81d40546590a9a
raulsb <rsorianobravo@gmail.com>
2025-05-19 15:59:10 +0200

    [ADD] BookModel Repository

---
+++ BookStoreCatalog_API/Repository/BookModelRepo.cs
@@ -0,0 +1,24 @@
+﻿using BookStoreCatalog_API.Data;
+using BookStoreCatalog_API.Models;
+using BookStoreCatalog_API.Repository.IRepository;
+
+namespace BookStoreCatalog_API.Repository
+{
+    public class BookModelRepo : Repository<BookModel>, IBookRepo
+    {
+        private readonly ApplicationDbContext _dbContext;
+
+        public BookModelRepo(ApplicationDbContext dbContext) :base(dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<BookModel> Update(BookModel entity)
+        {
+            entity.CreatedAt = DateTime.Now;
+            _dbContext.Books.Update(entity);
+            await _dbContext.SaveChangesAsync();
+            return entity;
+        }
+    }
+}
---
+++ BookStoreCatalog_API/Repository/IRepository/IBookRepo.cs
@@ -0,0 +1,9 @@
+﻿using BookStoreCatalog_API.Models;
+
+namespace BookStoreCatalog_API.Repository.IRepository
+{
+    public interface IBookRepo : IRepository<BookModel>
+    {
+        Task<BookModel> Update(BookModel entity);
+    }
+}
