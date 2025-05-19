56fc2171304557fddffbcad5d1a7fc9b9024dd1d
raulsb <rsorianobravo@gmail.com>
2025-05-19 09:35:54 +0200

    [ADD] Use DTOs in create and update/patch

--- BookStoreCatalog_API/Controllers/BookController.cs
+++ BookStoreCatalog_API/Controllers/BookController.cs
@@ -642,6 +642,175 @@ namespace BookStoreCatalog_API.Controllers
             _dbContext.SaveChanges();
             return NoContent();
         }
-    }
 
+        //----------------------------------------------
+        //            DTOS
+        //----------------------------------------------
+        /// <summary>
+        /// Create a new Book
+        /// </summary>
+        /// <param name="newBook"></param>
+        /// <returns>The result of the operation</returns>
+        [HttpPost("DbDTO/")]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public ActionResult<BookModel> CreateBookDbDTO([FromBody] BookModelCreateDTO newBook)
+        {
+
+            if (!ModelState.IsValid)
+            {
+                _logger.LogError("Error:" + ModelState.Values);
+                return BadRequest(ModelState);
+            }
+
+            if (newBook == null)
+            {
+                _logger.LogError("Error: Empty Book");
+                return BadRequest(newBook);
+            }
+
+            //--- Not necessary cause DTOs don't have Id
+            //if (newBook.Id > 0)
+            //{
+            //    _logger.LogError("Error: The ID is not valid");
+            //    return StatusCode(StatusCodes.Status500InternalServerError);
+            //}
+
+            else
+            {
+                try
+                {
+                    int a = 0;
+                    //if (_dbContext.Books.Count() > 0)
+                    //    newBook.Id = _dbContext.Books.OrderByDescending(x => x.Id).FirstOrDefault().Id + 1;
+
+                    //else newBook.Id = 1;
+
+                    if (_dbContext.Books.Any(book => book.Title.ToLower() == newBook.Title.ToLower()))
+                    {
+                        ModelState.AddModelError("SameBook", "This Book Exists, don't insist");
+                        _logger.LogError("Error:" + ModelState.ToList()[0].Value.Errors[0].ErrorMessage);
+                        return BadRequest(ModelState);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    return BadRequest(ex);
+                }
+
+            }
+
+            BookModel bookTemp = new()
+            {
+                idBook = newBook.idBook,
+                Title = newBook.Title,
+                TitleUrl = newBook.TitleUrl,
+                AuthorUrl = newBook.AuthorUrl,
+                Author = newBook.Author,
+                DescriptionUrl = newBook.DescriptionUrl,
+                Description = newBook.Description,
+                CreatedAt = newBook.CreatedAt
+            };
+
+            _dbContext.Books.Add(bookTemp);
+            _dbContext.SaveChanges();
+
+            return CreatedAtRoute("GetBookDb", new { id = bookTemp.Id }, bookTemp);
+        }
+
+        //----------------------------------------------
+        /// <summary>
+        /// Update a book
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="modBook"></param>
+        /// <returns>The result of the operation</returns>
+        [HttpPut("DbDTO/{id}:int")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+
+        public IActionResult UpdateBookDbDTO(int id, [FromBody] BookModelUpdateDTO modBook)
+        {
+            if (modBook == null || id != modBook.Id)
+            {
+                return BadRequest();
+            }
+
+            var book = _dbContext.Books.FirstOrDefault(book => book.Id == id);
+            if (book == null)
+            {
+                return NotFound();
+            }
+
+            book.idBook = modBook.idBook;
+            book.Author = modBook.Author;
+            book.Title = modBook.Title;
+            book.Description = modBook.Description;
+            book.AuthorUrl = modBook.AuthorUrl;
+            book.DescriptionUrl = modBook.DescriptionUrl;
+            book.TitleUrl = modBook.TitleUrl;
+
+            _dbContext.Books.Update(book);
+            _dbContext.SaveChanges();
+            return NoContent();
+        }
+
+        //----------------------------------------------
+        [HttpPatch("DbDTO/{id}:int")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+
+        public IActionResult UpdatePatchBookDbDTO(int id, JsonPatchDocument<BookModelUpdateDTO> patchBook)
+        {
+            if (patchBook == null || id == 0)
+            {
+                return BadRequest();
+            }
+
+            var book = _dbContext.Books.AsNoTracking().FirstOrDefault(book => book.Id == id);
+            if (book == null)
+            {
+                return NotFound();
+            }
+
+            BookModelUpdateDTO tempBook = new()
+            {
+                Id = book.Id,
+                idBook = book.idBook,
+                Title = book.Title,
+                TitleUrl = book.TitleUrl,
+                AuthorUrl = book.AuthorUrl,
+                Author = book.Author,
+                DescriptionUrl = book.DescriptionUrl,
+                Description = book.Description,
+                CreatedAt = book.CreatedAt
+            };
+
+            patchBook.ApplyTo(tempBook, ModelState);
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            BookModel bookTemp = new()
+            {
+                Id = tempBook.Id,
+                idBook = tempBook.idBook,
+                Title = tempBook.Title,
+                TitleUrl = tempBook.TitleUrl,
+                AuthorUrl = tempBook.AuthorUrl,
+                Author = tempBook.Author,
+                DescriptionUrl = tempBook.DescriptionUrl,
+                Description = tempBook.Description,
+                CreatedAt = tempBook.CreatedAt
+            };
+
+            _dbContext.Books.Update(bookTemp);
+            _dbContext.SaveChanges();
+            return NoContent();
+        }
+    }
 }
