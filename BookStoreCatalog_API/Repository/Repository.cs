d30edeee63f0949228f66ae537fe26b391d59306
raulsb <rsorianobravo@gmail.com>
2025-05-19 15:43:48 +0200

    [ADD] Reposritory Pattern with the interface

---
+++ BookStoreCatalog_API/Repository/Repository.cs
@@ -0,0 +1,63 @@
+﻿using BookStoreCatalog_API.Data;
+using BookStoreCatalog_API.Repository.IRepository;
+using Microsoft.EntityFrameworkCore;
+using System.Diagnostics;
+using System.Linq.Expressions;
+
+namespace BookStoreCatalog_API.Repository
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+
+        private readonly ApplicationDbContext _dbContext;
+        internal DbSet<T> dbSet { get; set; }
+
+        public Repository(ApplicationDbContext _dbContext)
+        {
+            _dbContext = _dbContext ?? throw new ArgumentNullException(nameof(_dbContext));
+            this.dbSet = _dbContext.Set<T>();
+        }
+
+        public async Task Create(T entity)
+        {
+            dbSet.AddAsync(entity);
+            await Save();
+        }
+
+        public async Task<T> Get(Expression<Func<T, bool>>? filter = null, bool tracked = true)
+        {
+            IQueryable<T> query = dbSet;
+            if (!tracked)
+            {
+                query = query.AsNoTracking();
+            }
+            if (filter != null)
+            { 
+                query = query.Where(filter);
+            }
+            return await query.FirstOrDefaultAsync();
+        }
+
+        public async Task<List<T>> GetAll(Expression<Func<T, bool>>? filter = null)
+        {
+            IQueryable<T> query = dbSet;
+
+            if (filter != null)
+            {
+                query = query.Where(filter);
+            }
+            return await query.ToListAsync();
+        }
+
+        public async Task Remove(T entity)
+        {
+            dbSet.Remove(entity);
+            await Save();
+        }
+
+        public async Task Save()
+        {
+            await _dbContext.SaveChangesAsync();
+        }
+    }
+}
