f2cfb7eab56f7bdaca99edab1439ed65b3abf6a2
raulsb <rsorianobravo@gmail.com>
2025-05-23 08:20:43 +0200

    [ADD] Added Models for Books and response

---
+++ BookStoreCatalog_web/Models/DTO/BookModelCreateDTO.cs
@@ -0,0 +1,40 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BookStoreCatalog_web.Models.DTO
+{
+    public class BookModelCreateDTO
+    {
+        #region PROPERTIES
+
+        //--- Book id
+        //public int Id { get; set; }
+
+        //--- Internal ID
+        public string idBook { get; set; }
+
+        //--- Book Title         
+        [Required]
+        [MaxLength(50)]
+        public string Title { get; set; }
+
+        //--- Book Description
+        public string Description { get; set; }
+
+        //--- Author
+        public string Author { get; set; }
+
+        //--- Authors Website
+        public string AuthorUrl { get; set; }
+
+        //--- Title Website
+        public string TitleUrl { get; set; }
+
+        //--- Description 
+        public string DescriptionUrl { get; set; }
+
+        //--- Date created
+        public DateTime CreatedAt { get; set; }
+
+        #endregion
+    }
+}
