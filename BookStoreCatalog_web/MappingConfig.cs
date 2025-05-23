db721d003bcdbb7ed504f3a6af842cd2e8a2392c
raulsb <rsorianobravo@gmail.com>
2025-05-23 09:40:22 +0200

    [ADD] Added AutoMapper and config class

---
+++ BookStoreCatalog_web/MappingConfig.cs
@@ -0,0 +1,23 @@
+﻿using AutoMapper;
+using BookStoreCatalog_web.Models.DTO;
+
+namespace BookStoreCatalog_web
+{
+    public class MappingConfig : Profile
+    {
+        public MappingConfig()
+        {
+            CreateMap<BookModelDTO, BookModelCreateDTO>();
+            CreateMap<BookModelCreateDTO, BookModelDTO>();
+
+
+            CreateMap<BookModelDTO, BookModelUpdateDTO>().ReverseMap();
+
+            //-------------------------------------------------
+
+            CreateMap<IssueModelDTO, IssueModelCreateDTO>().ReverseMap();
+            CreateMap<IssueModelDTO, IsssueModelUpdateDTO>().ReverseMap();
+
+        }
+    }
+}
