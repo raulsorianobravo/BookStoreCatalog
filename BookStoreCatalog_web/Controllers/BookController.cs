27363476230440a22f25a8810b06b7e12689078c
raulsb <rsorianobravo@gmail.com>
2025-05-23 11:20:12 +0200

    [ADD] Added Book controller

---
+++ BookStoreCatalog_web/Controllers/BookController.cs
@@ -0,0 +1,23 @@
+﻿using AutoMapper;
+using BookStoreCatalog_web.Services.IServices;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookStoreCatalog_web.Controllers
+{
+    public class BookController : Controller
+    {
+        private readonly IBookService _booService;
+        private readonly IMapper _mapper;
+
+        public BookController(IBookService bookService, IMapper mapper)
+        {
+            _booService = bookService;
+            _mapper = mapper;
+        }
+        
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
