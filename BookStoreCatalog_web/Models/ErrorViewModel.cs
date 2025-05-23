7e511fbf1488b68b55d2b1b5a323ef47017b7a23
raulsb <rsorianobravo@gmail.com>
2025-05-23 08:04:30 +0200

    [PROJECT] Added web project to the Solution

---
+++ BookStoreCatalog_web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BookStoreCatalog_web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
