02b3e895e7fce195c72772947fa895c336f9b180
raulsb <rsorianobravo@gmail.com>
2025-05-26 23:24:41 +0200

    [MOD] Modify the Index view

--- BookStoreCatalog_web/Controllers/HomeController.cs
+++ BookStoreCatalog_web/Controllers/HomeController.cs
@@ -1,5 +1,9 @@
+using AutoMapper;
 using BookStoreCatalog_web.Models;
+using BookStoreCatalog_web.Models.DTO;
+using BookStoreCatalog_web.Services.IServices;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 using System.Diagnostics;
 
 namespace BookStoreCatalog_web.Controllers
@@ -7,15 +11,29 @@ namespace BookStoreCatalog_web.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IBookService _bookService;
+        private readonly IMapper _mapper;
 
-        public HomeController(ILogger<HomeController> logger)
+
+        public HomeController(ILogger<HomeController> logger, IBookService bookService, IMapper mapper)
         {
             _logger = logger;
+            _bookService = bookService;
+            _mapper = mapper;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            return View();
+            List<BookModelDTO> bookList = new List<BookModelDTO>();
+
+            var response = await _bookService.GetAllBooks<APIResponse>();
+
+            if (response != null && response.IsSuccess)
+            {
+                bookList = JsonConvert.DeserializeObject<List<BookModelDTO>>(Convert.ToString(response.Result));
+            }
+
+            return View(bookList);
         }
 
         public IActionResult Privacy()
