3920bd9186673ee8b3b434cc59e99f1c3eb368c4
raulsb <rsorianobravo@gmail.com>
2025-05-27 08:40:02 +0200

    [ADD] Added create issue method and inject the service of book model

--- BookStoreCatalog_web/Controllers/IssueController.cs
+++ BookStoreCatalog_web/Controllers/IssueController.cs
@@ -3,7 +3,9 @@ using BookStoreCatalog_web.Models;
 using BookStoreCatalog_web.Models.DTO;
 using BookStoreCatalog_web.Services;
 using BookStoreCatalog_web.Services.IServices;
+using BookStoreCatalog_web.ViewModel;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Newtonsoft.Json;
 
 namespace BookStoreCatalog_web.Controllers
@@ -13,11 +15,13 @@ namespace BookStoreCatalog_web.Controllers
 
         private readonly IIssueService _issueService;
         private readonly IMapper _mapper;
+        private readonly IBookService _bookService;
 
-        public IssueController(IIssueService issueService, IMapper mapper)
+        public IssueController(IIssueService issueService, IMapper mapper, IBookService bookService)
         {
             _issueService = issueService;
             _mapper = mapper;
+            _bookService = bookService;
         }
 
         public IActionResult Index()
@@ -36,6 +40,25 @@ namespace BookStoreCatalog_web.Controllers
             }
 
             return View(issueList);
+        }
+
+        public async Task<IActionResult> CreateIssue()
+        {
+            
+            IssueViewModel issueViewModel = new IssueViewModel();
+            var response = await _bookService.GetAllBooks<APIResponse>();
+
+            if(response != null && response.IsSuccess)
+            {
+                issueViewModel.BookList = JsonConvert.DeserializeObject<List<BookModelDTO>>(Convert.ToString(response.Result))
+                    .Select(v => new SelectListItem
+                    {
+                        Text = v.Title,
+                        Value = v.Id.ToString()
+                    });
+            }
+            
+            return View(issueViewModel);
         }
     }
 }
---
+++ BookStoreCatalog_web/ViewModel/IssueViewModel.cs
@@ -0,0 +1,16 @@
+﻿using BookStoreCatalog_web.Models.DTO;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace BookStoreCatalog_web.ViewModel
+{
+    public class IssueViewModel
+    {
+        public IssueModelDTO Issue { get; set; }
+        public IEnumerable<SelectListItem> BookList { get; set; }
+
+        public IssueViewModel()
+        {
+            Issue = new IssueModelDTO();
+        }
+    }
+}
