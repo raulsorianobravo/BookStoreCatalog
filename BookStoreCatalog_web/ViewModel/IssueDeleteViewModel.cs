11c39b3a230666f2e1bee1424c87205626d93b3a
raulsb <rsorianobravo@gmail.com>
2025-06-03 16:27:27 +0200

    [ADD] Added issue delete viewmodel

---
+++ BookStoreCatalog_web/ViewModel/IssueDeleteViewModel.cs
@@ -0,0 +1,17 @@
+﻿using BookStoreCatalog_web.Models.DTO;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace BookStoreCatalog_web.ViewModel
+{
+    public class IssueDeleteViewModel
+    {
+
+        public IssueModelDTO Issue { get; set; }
+        public IEnumerable<SelectListItem> BookList { get; set; }
+
+        public IssueDeleteViewModel()
+        {
+            Issue = new IssueModelDTO();
+        }
+    }
+}
