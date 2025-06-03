c2e2e85bd5926394f1bbee0cf93584a5af07c53e
raulsb <rsorianobravo@gmail.com>
2025-06-03 13:09:40 +0200

    [ADD] Added IssueViewModel for updates

---
+++ BookStoreCatalog_web/ViewModel/IssueUpdateViewModel.cs
@@ -0,0 +1,16 @@
+﻿using BookStoreCatalog_web.Models.DTO;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace BookStoreCatalog_web.ViewModel
+{
+    public class IssueUpdateViewModel
+    {
+        public IsssueModelUpdateDTO Issue { get; set; }
+        public IEnumerable<SelectListItem> BookList { get; set; }
+
+        public IssueUpdateViewModel()
+        {
+            Issue = new IsssueModelUpdateDTO();
+        }
+    }
+}
