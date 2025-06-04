215ec49cfff157642d6d6d765decd9f87b4ea5dc
raulsb <rsorianobravo@gmail.com>
2025-06-04 10:44:53 +0200

    [ADD] Create User model for Auth

---
+++ BookStoreCatalog_API/Models/UserModel.cs
@@ -0,0 +1,19 @@
+﻿namespace BookStoreCatalog_API.Models
+{
+    public class UserModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string Username { get; set; }
+        public string PasswordHash { get; set; }
+        public string PasswordSalt { get; set; }
+        public string Role { get; set; }
+        public DateTime CreatedAT { get; set; }
+        public DateTime UpdatedAT { get; set; }
+        public DateTime LastUpdatedAT { get; set; }
+        public DateTime LastUpdatedBy { get; set; }
+
+    }
+}
