d03eed8212d99229596cc73157d6990665486f29
raulsb <rsorianobravo@gmail.com>
2025-06-04 11:08:09 +0200

    [ADD] Added interface repository for User

---
+++ BookStoreCatalog_API/Repository/IRepository/IUserRepo.cs
@@ -0,0 +1,14 @@
+﻿using BookStoreCatalog_API.Models;
+using BookStoreCatalog_API.Models.DTO;
+
+namespace BookStoreCatalog_API.Repository.IRepository
+{
+    public interface IUserRepo 
+    {
+        bool IsUserUnique(string username);
+
+        Task<LoginResponseDTO> Login(LoginRequestDTO loginRequestDTO);
+
+        Task<UserModel> Register(RegisterRequestDTO registerRequestDTO);
+    }
+}
