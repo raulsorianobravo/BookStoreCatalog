7bf8f8a7f7f1057121bf391430e4c9ec1da76155
raulsb <rsorianobravo@gmail.com>
2025-06-04 22:08:38 +0200

    [FIX] Initialize the list

--- BookStoreCatalog_API/Models/APIResponse.cs
+++ BookStoreCatalog_API/Models/APIResponse.cs
@@ -4,10 +4,16 @@ namespace BookStoreCatalog_API.Models
 {
     public class APIResponse
     {
-
         public HttpStatusCode StatusCode { get; set; }
         public bool IsSuccess { get; set; }
         public List<string> ErrorMessages { get; set; }
         public object Result { get; set; }
+        
+        public APIResponse() 
+        { 
+            ErrorMessages = new List<string>();
+        }
+
+
     }
 }
