bf90e12acac2198aeb5ae98a10c3d08f045e96af
raulsb <rsorianobravo@gmail.com>
2025-06-04 10:46:11 +0200

    [ADD] Create the DTO models for Sign in and Register

---
+++ BookStoreCatalog_API/Models/DTO/LoginResponseDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace BookStoreCatalog_API.Models.DTO
+{
+    public class LoginResponseDTO
+    {
+        public UserModel User { get; set; }
+        public string Token {  get; set; }
+    }
+}
---
+++ BookStoreCatalog_API/Models/DTO/RegisterRequestDTO.cs
@@ -0,0 +1,18 @@
+﻿namespace BookStoreCatalog_API.Models.DTO
+{
+    public class RegisterRequestDTO
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string Username { get; set; }
+        public string PasswordHash { get; set; }
+        public string PasswordSalt { get; set; }
+        public string Role { get; set; }
+        public DateTime CreatedAT { get; set; }
+        public DateTime UpdatedAT { get; set; }
+        public DateTime LastUpdatedAT { get; set; }
+        public DateTime LastUpdatedBy { get; set; }
+
+    }
+}
