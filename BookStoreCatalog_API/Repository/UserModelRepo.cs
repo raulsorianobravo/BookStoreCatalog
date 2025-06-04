bf1e72d993ecf382d3c9af4e5e86423fae9a4b9b
raulsb <rsorianobravo@gmail.com>
2025-06-04 11:08:35 +0200

    [ADD] Added UserModel repository implementing the interface

---
+++ BookStoreCatalog_API/Repository/UserModelRepo.cs
@@ -0,0 +1,53 @@
+﻿using BookStoreCatalog_API.Data;
+using BookStoreCatalog_API.Models;
+using BookStoreCatalog_API.Models.DTO;
+using BookStoreCatalog_API.Repository.IRepository;
+
+namespace BookStoreCatalog_API.Repository
+{
+    public class UserModelRepo : IUserRepo
+    {
+        private readonly ApplicationDbContext _context;
+        public UserModelRepo(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public bool IsUserUnique(string username)
+        {
+            var user = _context.Users.FirstOrDefault(x => x.Username.ToLower() == username.ToLower());
+            if (user == null) 
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public Task<LoginResponseDTO> Login(LoginRequestDTO loginRequestDTO)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<UserModel> Register(RegisterRequestDTO registerRequestDTO)
+        {
+            UserModel user = new UserModel();
+            user.Username = registerRequestDTO.Username;
+            user.Password = registerRequestDTO.Password;
+            user.Email = registerRequestDTO.Email;
+            user.Role = registerRequestDTO.Role;
+            user.PasswordHash = registerRequestDTO.PasswordHash;
+            user.PasswordSalt = registerRequestDTO.PasswordSalt;
+            user.Name = registerRequestDTO.Name;
+            user.CreatedAT = DateTime.Now;
+            user.UpdatedAT = DateTime.Now;
+            user.LastUpdatedAT = DateTime.Now;
+            user.LastUpdatedBy = DateTime.Now;
+
+            await _context.AddAsync(user);
+            await _context.SaveChangesAsync();
+
+            user.Password = "";
+            return user;
+        }
+    }
+}
