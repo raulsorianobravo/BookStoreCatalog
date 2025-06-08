1f26a881bee144c57973d2c1a002842e65a1d3c7
raulsb <rsorianobravo@gmail.com>
2025-06-08 08:15:34 +0200

    [ADD] Create user controller

---
+++ BookStoreCatalog_web/Controllers/UserController.cs
@@ -0,0 +1,63 @@
+﻿using BookStoreCatalog_web.Models;
+using BookStoreCatalog_web.Models.DTO;
+using BookStoreCatalog_web.Services.IServices;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookStoreCatalog_web.Controllers
+{
+    public class UserController : Controller
+    {
+        private readonly IUserService _userService;
+        public UserController(IUserService userService)
+        {
+            _userService = userService;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Login() 
+        { 
+            return View(); 
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Login(LoginRequestDTO userDTO) 
+        { 
+            return View(); 
+        }
+        
+        public IActionResult Logout() 
+        { 
+            return View(); 
+        }
+
+        public IActionResult Forbidden()
+        {
+            return View();
+        }
+
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Register(RegisterRequestDTO userDTO)
+        {
+            var response = await _userService.Register <APIResponse>(userDTO);
+
+            if (response != null && response.IsSuccess) 
+            {
+                return RedirectToAction("login");
+            }
+            return View();
+        }
+
+
+    }
+}
