f9ca7084500b1945a60419971c4174b2a740b452
raulsb <rsorianobravo@gmail.com>
2025-06-08 07:59:55 +0200

    [ADD] Create user service interface and the implementation

---
+++ BookStoreCatalog_web/Services/IServices/IUserService.cs
@@ -0,0 +1,11 @@
+﻿using BookStoreCatalog_web.Models.DTO;
+
+namespace BookStoreCatalog_web.Services.IServices
+{
+    public interface IUserService
+    {
+        Task<T> Login<T>(LoginRequestDTO dto);
+
+        Task<T> Register<T>(RegisterRequestDTO dto);
+    }
+}
---
+++ BookStoreCatalog_web/Services/UserService.cs
@@ -0,0 +1,40 @@
+﻿using BookStoreCatalog_web.Models;
+using BookStoreCatalog_web.Models.DTO;
+using BookStoreCatalog_web.Services.IServices;
+using BookStoreCatalogUtils;
+
+namespace BookStoreCatalog_web.Services
+{
+    public class UserService : BaseService, IUserService
+    {
+
+        public readonly IHttpClientFactory _httpClient;
+        private string _userURL;
+
+        public UserService(IHttpClientFactory httpClient, IConfiguration configuration) :base(httpClient)
+        {
+            _httpClient = httpClient;
+            _userURL = configuration.GetValue<string>("ServiceURLs:API_URL");
+        }
+
+        public Task<T> Login<T>(LoginRequestDTO userDTO)
+        {
+            return SendAsync<T>(new APIRequest()
+            {
+                APIType = ClassDefinitions.APIType.POST,
+                Data = userDTO,
+                URL = _userURL + "/api/user/login"
+            });
+        }
+
+        public Task<T> Register<T>(RegisterRequestDTO userDTO)
+        {
+            return SendAsync<T>(new APIRequest()
+            {
+                APIType = ClassDefinitions.APIType.POST,
+                Data = userDTO,
+                URL = _userURL + "/api/user/register"
+            });
+        }
+    }
+}
