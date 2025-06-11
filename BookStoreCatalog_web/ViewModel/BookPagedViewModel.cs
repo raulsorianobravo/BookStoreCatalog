dcf6d67755a3b06849a8fc2605cd0eb7c8cfc206
raulsb <rsorianobravo@gmail.com>
2025-06-11 17:50:33 +0200

    [ADD] Create view model for the result paged

---
+++ BookStoreCatalog_web/ViewModel/BookPagedViewModel.cs
@@ -0,0 +1,19 @@
+﻿using BookStoreCatalog_web.Models.DTO;
+using System.Diagnostics.Eventing.Reader;
+
+namespace BookStoreCatalog_web.ViewModel
+{
+    public class BookPagedViewModel
+    {
+        public int PageNumber { get; set; }
+        public int PageSize { get; set; }
+
+        public string Prev { get; set; } = "disable";
+
+        public string Next { get; set; } = "";
+
+        public IEnumerable<BookModelDTO> BookList { get; set; }
+
+
+    }
+}
